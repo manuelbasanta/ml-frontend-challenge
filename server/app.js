0a49808982632dd13b544bafbe402049f1374603
manuelbasanta <manuelbasanta1@gmail.com>
2019-10-06 23:21:23 -0300

    fix routes

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ const itemsRouter = require('./controllers/items');
 
 const middleware = require('./utils/middleware');
 
-const allowedDomain = "http://localhost:3000";
+const allowedDomain = 'http://localhost:3000';
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", `${allowedDomain}`);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', `${allowedDomain}`);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
